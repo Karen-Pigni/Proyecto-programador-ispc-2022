34bf7b7a111aa1382b566adb0d5f674234348ecd
Karen Pigni <64334155+Karen-Pigni@users.noreply.github.com>
2022-08-11 18:51:15 -0300

    modificación en ejercicio .sql

--- ejercicio9.sql
+++ ejercicio9.sql
@@ -1,4 +1,4 @@
 -- 9.Escriba una consulta que permita actualizar la dirección de un dueño. Su nueva dirección es Libertad 123
 
 UPDATE Dueno 
-SET Dirección = 'Libertad 123' WHERE DNI = 123456789;
+SET Dirección = 'Libertad 123' WHERE DNI = 66666666;
